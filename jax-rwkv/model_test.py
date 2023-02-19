380427fb6215681f7fee26f75957eb09e1c1388c
tensorpro <tensorpro@gmail.com>
2023-02-19 10:45:55 -0800

    fix test path

--- jax-rwkv/model_test.py
+++ jax-rwkv/model_test.py
@@ -6,15 +6,7 @@ import convert
 
 device = 'cpu'
 
-def check_keys_match(a, b, match_fn):
-    for k, v in b.items():
-        if v == None:
-            continue
-        else:
-            match_fn(v, b[k])
-
-
-# testdata has serialized dictionaries, each with the
+# the jax-rwkv/testdata directoryhas serialized dictionaries, each with the
 # following schema:
 # 
 # `model_weights` is a state_dict for a torch module
@@ -27,7 +19,7 @@ class TestModel(unittest.TestCase):
         # loads test data with a torch model with:
         #    tiny_att_dim=7, tiny_att_layer=5, head_qk=11
         #    vocab_size=6, n_layer=10, n_embd=5, pre_ffn=1
-        test_data = torch.load('./testdata/small-model-test.pt')
+        test_data = torch.load(f'./jax-rwkv/testdata/small-model-test.pt')
         params, config = convert.rwkv(test_data['model_state_dict'])
         rwkv = model.BatchRWKV(config)
         jax_out = rwkv.apply(params, test_data['input'].numpy())
