95b6444525ee2897a0f635f314be329937b171af
tensorpro <tensorpro@gmail.com>
2023-04-01 13:12:41 -0700

    add hidden state output

--- tpu_rwkv/model.py
+++ tpu_rwkv/model.py
@@ -1,11 +1,14 @@
 import math
 from dataclasses import dataclass, field
-from typing import Any, List, Type, Union, Optional
+from functools import partial
+from itertools import repeat
+from typing import Any, List, Optional
 
+import flax
+from flax.training import checkpoints
 import jax
 import jax.numpy as jnp
 from flax import linen as nn
-
 
 
 @dataclass
@@ -40,7 +43,8 @@ class Config:
     )
     attention_at_layer: int = field(
         default=-1,
-        metadata={"help": "which layer we should add a small attention too. If negative, no attention is included."},
+        metadata={
+            "help": "which layer we should add a small attention too. If negative, no attention is included."},
     )
     attention_size: int = field(
         default=-1,
@@ -52,11 +56,13 @@ class Config:
     )
     channel_mix_at_input: bool = field(
         default=False,
-        metadata={"help": "whether we should use a channel mix instead of time mix at the input"},
+        metadata={
+            "help": "whether we should use a channel mix instead of time mix at the input"},
     )
     pos_embedding_size: int = field(
         default=0,
-        metadata={"help": "(Only for vision experiments): size of position embedding"},
+        metadata={
+            "help": "(Only for vision experiments): size of position embedding"},
     )
     dtype: Optional[str] = field(
         default="bfloat16",
@@ -69,42 +75,6 @@ class Config:
     )
 
 
-def wkv_single_channel(wc, uc, kc, vc, state_in_c=(0,0,-jnp.inf)):
-    """
-    credit for this kernel: github.com/BlealTan
-    """
-
-    def step(pqo, kct_vct):
-        kct, vct = kct_vct
-
-        p, q, o = pqo
-        no = jnp.maximum(o, uc + kct)
-        A = jnp.exp(o - no)
-        B = jnp.exp(uc + kct - no)
-        y = (A * p + B * vct) / (A * q + B)
-
-        no = jnp.maximum(wc + o, kct)
-        A = jnp.exp(wc + o - no)
-        B = jnp.exp(kct - no)
-        p = A * p + B * vct
-        q = A * q + B
-        o = no
-
-        return (p, q, o), y
-
-    state_out_c, y = jax.lax.scan(step, state_in_c, (kc, vc))
-    return y, state_out_c
-
-@jax.jit
-def wkv(w,u,k,v):
-    w = -jnp.exp(w)
-    return jax.vmap(wkv_single_channel, -1, -1)(w,u,k,v)[0]
-
-@jax.jit
-def time_shift(x):
-    return jnp.pad(x, [(1, 0), (0, 0)])[:-1, :]
-
-
 def initialize_to_value(x, dtype):
     """
     makes an initializer function that ignores the given PRNGKey
@@ -112,18 +82,16 @@ def initialize_to_value(x, dtype):
     """
     return lambda _: x.astype(dtype)
 
+
 class TimeMix(nn.Module):
-    config: Config
     layer_depth: int
+    config: Config
 
     def setup(self):
-        """
-        Initializes a time mix module for a given config / layer depth.
-        """
-        config = self.config
         layer_depth = self.layer_depth
-        embedding_size = config.embedding_size
-        num_layers = config.num_layers
+        embedding_size = self.config.embedding_size
+        num_layers = self.config.num_layers
+        dtype = self.config.dtype
 
         # goes from 0 to 1 along layer depth
         ratio_0_to_1 = layer_depth / (num_layers - 1)
@@ -131,98 +99,141 @@ class TimeMix(nn.Module):
         ratio_1_to_almost_0 = 1.0 - (layer_depth / num_layers)
         zigzag = .5 * (jnp.arange(1, embedding_size+1) % 3 - 1)
         time_first = jnp.full(embedding_size, math.log(.3)) + zigzag
-        self.time_first = self.param('time_first', initialize_to_value(time_first, config.dtype))
+        self.time_first = self.param(
+            'time_first', initialize_to_value(time_first, 'float32'))
+
+        h = jnp.arange(0, embedding_size)
+        # the numbers used here were found to work well from experiments
+        time_decay = -5 + 8 * (h / (embedding_size - 1)
+                               ) ** (.7 + 1.3 * ratio_0_to_1)
+
+        self.time_decay = self.param(
+            'time_decay', initialize_to_value(time_decay, 'float32'))
 
         x = (jnp.arange(embedding_size) /
-             embedding_size)[jnp.newaxis, :]
+             embedding_size)
         time_mix_k = jnp.power(x, ratio_1_to_almost_0)
         time_mix_v = time_mix_k + .3 * ratio_0_to_1
         time_mix_r = jnp.power(x, .5 * ratio_1_to_almost_0)
+        self.time_mix_k = self.param(
+            'time_mix_k', initialize_to_value(time_mix_k, dtype))
+        self.time_mix_v = self.param(
+            'time_mix_v', initialize_to_value(time_mix_v, dtype))
+        self.time_mix_r = self.param(
+            'time_mix_r', initialize_to_value(time_mix_r, dtype))
 
-        self.time_mix_k = self.param('time_mix_k', initialize_to_value(time_mix_k, config.dtype))
-        self.time_mix_v = self.param('time_mix_v', initialize_to_value(time_mix_v, config.dtype))
-        self.time_mix_r = self.param('time_mix_r', initialize_to_value(time_mix_r, config.dtype))
+        self.layernorm = nn.LayerNorm(epsilon=1e-5, dtype=dtype)
+        self.key = nn.Dense(embedding_size, use_bias=False, dtype=dtype)
+        self.value = nn.Dense(embedding_size, use_bias=False, dtype=dtype)
+        self.receptance = nn.Dense(embedding_size, use_bias=False, dtype=dtype)
+        self.output = nn.Dense(embedding_size, use_bias=False, dtype=dtype)
 
-        h = jnp.arange(0, embedding_size)
-        decay_speed = -5 + 8 * (h / (embedding_size - 1)
-                                ) ** (.7 + 1.3 * ratio_0_to_1)
-        self.time_decay = self.param('time_decay', initialize_to_value(decay_speed, config.dtype))
-        self.key = nn.Dense(embedding_size, use_bias=False, dtype=config.dtype)
-        self.value = nn.Dense(embedding_size, use_bias=False, dtype=config.dtype)
-        self.receptance = nn.Dense(embedding_size, use_bias=False, dtype=config.dtype)
-        self.output = nn.Dense(embedding_size, use_bias=False, dtype=config.dtype)
+    def __call__(self, x, time_mix_state):
+        sx, aa, bb, pp = time_mix_state
+        xx = self.layernorm(x)
+        sx = jnp.concatenate((jnp.expand_dims(sx, 0), xx[:-1, :]))
+        kx = xx * self.time_mix_k + sx * (1 - self.time_mix_k)
+        vx = xx * self.time_mix_v + sx * (1 - self.time_mix_v)
+        rx = xx * self.time_mix_r + sx * (1 - self.time_mix_r)
+        r = nn.sigmoid(self.receptance(rx))
+        k = self.key(kx)
+        v = self.value(vx)
+        T = x.shape[0]
 
-    @nn.jit
-    def __call__(self, x):
-        T, C = x.shape
+        def step(state, kv):
+            (aa, bb, pp), (kk, vv) = state, kv
+            ww = self.time_first + kk
+            p = jnp.maximum(pp, ww)
+            e1 = jnp.exp(pp - p)
+            e2 = jnp.exp(ww - p)
+            out = ((e1 * aa + e2 * vv) / (e1 * bb + e2)).astype(dtype=r.dtype)
 
-        # performs time_shift.
-        xx = time_shift(x)
-        xk = x * self.time_mix_k + xx * (1 - self.time_mix_k)
-        xv = x * self.time_mix_v + xx * (1 - self.time_mix_v)
-        xr = x * self.time_mix_r + xx * (1 - self.time_mix_r)
-        k = self.key(xk)
-        v = self.value(xv)
-        r = self.receptance(xr)
-        sr = jax.nn.sigmoid(r)
-        rwkv = sr * wkv(self.time_decay, self.time_first, k, v)
-        rwkv = self.output(rwkv)
-        return rwkv
+            ww = -jnp.exp(self.time_decay) + pp
+            p = jnp.maximum(ww, kk)
+            e1 = jnp.exp(ww - p)
+            e2 = jnp.exp(kk - p)
+            aa = e1 * aa + e2 * vv
+            bb = e1 * bb + e2
+            pp = p
+            return (aa, bb, pp), out
+
+        (aa, bb, pp), sx = jax.lax.scan(step, (aa, bb, pp), (k, v))
+        out = x + self.output(r * sx)
+        # (xx[-1,:], aa, bb, pp) is the next time_mix_state
+        return out, (xx[-1, :], aa, bb, pp)
 
 
 class ChannelMix(nn.Module):
-    config: Config
     layer_depth: int
+    config: Config
 
     def setup(self):
-        layer_number = self.layer_depth
-        config = self.config
-        ratio_1_to_almost0 = 1.0 - (layer_number / config.num_layers)
-
-        embedding_size = config.embedding_size
-        x = (jnp.arange(embedding_size) /
-             embedding_size)[jnp.newaxis, ...]
-
-        self.time_mix_k = jnp.power(x, ratio_1_to_almost0)
-        self.time_mix_r = jnp.power(x, ratio_1_to_almost0)
+        layer_depth = self.layer_depth
+        num_layers = self.config.num_layers
+        embedding_size = self.config.embedding_size
+        dtype = self.config.dtype
 
         hidden_size = 4 * embedding_size
-        self.key = nn.Dense(hidden_size, use_bias=False, dtype=config.dtype)
-        self.receptance = nn.Dense(embedding_size, use_bias=False, dtype=config.dtype)
-        self.value = nn.Dense(embedding_size, use_bias=False, dtype=config.dtype)
+        self.layernorm = nn.LayerNorm(epsilon=1e-5, dtype=dtype)
+        self.key = nn.Dense(hidden_size, use_bias=False, dtype=dtype)
+        self.receptance = nn.Dense(embedding_size, use_bias=False, dtype=dtype)
+        self.value = nn.Dense(embedding_size, use_bias=False, dtype=dtype)
 
-    @nn.jit
-    def __call__(self, x):
-        xx = time_shift(x)
-        xk = x * self.time_mix_k + xx * (1 - self.time_mix_k)
-        xr = x * self.time_mix_r + xx * (1 - self.time_mix_r)
-        k = self.key(xk)
-        k = jnp.square(jax.nn.relu(k))
-        kv = self.value(k)
-        rkv = jax.nn.sigmoid(self.receptance(xr)) * kv
-        return rkv
+        x = (jnp.arange(embedding_size) /
+             embedding_size)
 
+        ratio_1_to_almost_0 = 1.0 - (layer_depth / num_layers)
+        time_mix_k = jnp.power(x, ratio_1_to_almost_0)
+        time_mix_r = jnp.power(x, .5 * ratio_1_to_almost_0)
+        self.time_mix_k = self.param(
+            'time_mix_k', initialize_to_value(time_mix_k, dtype))
+        self.time_mix_r = self.param(
+            'time_mix_r', initialize_to_value(time_mix_r, dtype))
+
+    def __call__(self, x, channel_mix_state):
+        xx = self.layernorm(x)
+        sx = jnp.concatenate(
+            (jnp.expand_dims(channel_mix_state, 0), xx[:-1, :]))
+        xk = xx * self.time_mix_k + sx * (1 - self.time_mix_k)
+        xr = xx * self.time_mix_r + sx * (1 - self.time_mix_r)
+        r = nn.sigmoid(self.receptance(xr))
+        k = jnp.square(nn.relu(self.key(xk)))
+        out = x + r * self.value(k)
+
+        return out, xx[-1, :]
+
+def empty_state(embedding_size):
+    "returns an empty block_state for a given embedding size"
+    zeros = jnp.zeros(embedding_size)
+    min_values = jnp.full(embedding_size, -jnp.inf)
+    time_mix_state = (zeros, zeros, zeros, min_values)
+    channel_mix_state = zeros
+    return time_mix_state, channel_mix_state
 
 class Block(nn.Module):
-    config: Config
     layer_num: int
-    mix1_type: Union[Type[ChannelMix], Type[TimeMix]] = TimeMix
+    config: Config
 
     def setup(self):
-        config, layer_num = self.config, self.layer_num
-        embedding_size = config.embedding_size
-        position_embedding_size = config.pos_embedding_size
-        self.layer_norm1 = nn.LayerNorm(epsilon=1e-5, dtype=config.dtype)
-        self.layer_norm2 = nn.LayerNorm(epsilon=1e-5, dtype=config.dtype)
-        self.mix1 = self.mix1_type(config, layer_num)
-        self.mix2 = ChannelMix(config, layer_num)
+        self.time_mix = TimeMix(self.layer_num, self.config)
+        self.channel_mix = ChannelMix(self.layer_num, self.config)
 
-    @nn.jit
-    def __call__(self, x):
-        T, C = x.shape
-        x = x + self.mix1(self.layer_norm1(x))
-        x = x + self.mix2(self.layer_norm2(x))
-        return x
+    def __call__(self, x, block_state):
+        """
+        Takes the embedding from the previous layer, and the `block_state`
+        from the previous time_step.
+
+        `block_state` is a tuple of `time_mix_state` and `channel_mix_state`,
+        which are used as inputs to the block's `time_mix` and `channel_mix`
+        respectively.
+        """
+        if block_state is None:
+            block_state = empty_state(self.config.embedding_size)
+
+        time_mix_state, channel_mix_state = block_state
+        x, time_mix_state = self.time_mix(x, time_mix_state)
+        x, channel_mix_state = self.channel_mix(x, channel_mix_state)
+        return x, (time_mix_state, channel_mix_state)
 
 
 class TinyAttention(nn.Module):
@@ -276,8 +287,10 @@ class HeadQK(nn.Module):
     def setup(self):
         config = self.config
         context_length = config.context_length
-        self.head_q = nn.Dense(self.config.head_qk_size, use_bias=False, dtype=config.dtype)
-        self.head_k = nn.Dense(self.config.head_qk_size, use_bias=False, dtype=config.dtype)
+        self.head_q = nn.Dense(self.config.head_qk_size,
+                               use_bias=False, dtype=config.dtype)
+        self.head_k = nn.Dense(self.config.head_qk_size,
+                               use_bias=False, dtype=config.dtype)
         self.copy_mask = jnp.tril(
             jnp.ones((context_length, context_length))) == 1
 
@@ -298,47 +311,83 @@ class RWKV(nn.Module):
 
     def setup(self):
         config = self.config
-        self.emb = nn.Embed(config.vocab_size, config.embedding_size, dtype=config.dtype)
-        self.ln_in = nn.LayerNorm(epsilon=1e-5, dtype=config.dtype)
+        self.embed = nn.Embed(config.vocab_size,
+                              config.embedding_size,
+                              config.dtype)
+        self.input_layernorm = nn.LayerNorm(epsilon=1e-5, dtype=config.dtype)
+        self.blocks = [Block(i, self.config) for i in range(config.num_layers)]
+        self.output_layernorm = nn.LayerNorm(epsilon=1e-5, dtype=config.dtype)
+        self.head = nn.Dense(self.config.vocab_size, use_bias=False)
 
-        if 0 <= config.attention_at_layer < config.num_layers:
-            self.tiny_attention = TinyAttention(config)
+    def __call__(self, x, block_states=None, output_state=False):
+        # we use a helper method for the forward pass until
+        # nn.jit supports static_argnames
+        return self.forward_with_state(x, block_states, output_state)
+    
+    @partial(nn.jit, static_argnums=3)
+    def forward_with_state(self, x, block_states, output_state):
+        x = self.embed(x)
+        x = self.input_layernorm(x)
 
-        blocks = []
-        for i in range(config.num_layers):
-            mix = ChannelMix if i == 0 and config.channel_mix_at_input else TimeMix
-            blocks.append(Block(config, i, mix))
-        self.blocks = blocks
+        next_states = []
+        if block_states is None:
+            block_states = repeat(None)
+        for block, state in zip(self.blocks, block_states):
+            x, new_state = block(x, state)
+            if output_state:
+                next_states.append(new_state)
 
-        self.ln_out = nn.LayerNorm(epsilon=1e-5, dtype=config.dtype)
-        self.head = nn.Dense(config.vocab_size, use_bias=False, dtype=config.dtype)
+        x = self.output_layernorm(x)
+        x = self.head(x)
 
-        # self.head_qk = lambda *_: 0.
-        if config.head_qk_size > 0:
-            self.head_qk = HeadQK(config)
-
-    @nn.jit
-    def __call__(self, idx):
-        config = self.config
-        T = len(idx)
-        assert T <= config.context_length, "Cannot forward, model ctx_len is exhausted."
-        x = self.emb(idx)
-        x_emb = x
-
-        x = self.ln_in(x)
-        for i, block in enumerate(self.blocks):
-            x = block(x)
-            if config.attention_at_layer == i:
-                x = self.tiny_attention(x, x_emb)
-        x = self.ln_out(x)
-
-        c = 0
-        if config.head_qk_size:
-            c = self.head_qk(x, idx)
-        x = self.head(x) + c
+        if output_state:
+            return x, next_states
         return x
 
-
 BatchRWKV = nn.vmap(RWKV, in_axes=0, out_axes=0,
-                    variable_axes={'params': None},
-                    split_rngs={'params': False})
+                    variable_axes={ 'params' : None },
+                    split_rngs={ 'params' : False })
+
+def save_weights(output_path, params, step):
+    checkpoints.save_checkpoint(output_path, params, step)
+
+
+def load_weights(path:str, dtype='float32', embedding_padding=0):
+    import torch
+    ws = {k: v.float() for k, v in torch.load(path, 'cpu').items()}
+    out = {}
+    for path, weight in ws.items():
+        path = path.replace('emb.weight', 'embed.embedding')
+        if 'ln' in path:
+            path = path.replace('weight', 'scale')
+        else:
+            path = path.replace('weight', 'kernel')
+
+        path = path.replace('blocks.0.ln0', 'input_layernorm')
+        path = path.replace('ln1', 'time_mix.layernorm')
+        path = path.replace('ln2', 'channel_mix.layernorm')
+        path = path.replace('ffn', 'channel_mix')
+        path = path.replace('att', 'time_mix')
+        path = path.replace('ln_out', 'output_layernorm')
+        path = path.replace('blocks.', 'blocks_')
+        if 'time_mix_' in path and len(weight.shape) > 1:
+            weight = weight.resize(weight.shape[-1])
+        if 'kernel' in path and len(weight.shape) > 1:
+            weight = weight.T
+
+        if 'time_decay' in path or 'time_first' in path:
+            dtype = 'float32'
+        if 'embedding' in path:
+            V, C = weight.shape
+            padded = torch.zeros(V+embedding_padding, C)
+            padded[:V] = weight
+            weight = padded
+        if 'head' in path:
+            C, V = weight.shape
+            padded = torch.zeros(C, V+embedding_padding)
+            padded[:, :V] = weight
+            weight = padded
+        out[path] = jnp.array(weight.detach().numpy(), dtype)
+
+    out = {'params': flax.traverse_util.unflatten_dict(out, sep='.')}
+    return out
